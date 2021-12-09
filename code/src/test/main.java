854f3ca9e17514dfb0ccd0a42cccc4068cdfa2c9
Salaheldinyoussry <salaheldeenyousryabdallah@gmail.com>
2021-12-09 19:12:32 +0200

    final

--- code/src/test/main.java
+++ code/src/test/main.java
@@ -1,9 +1,41 @@
 package test;
 
 public class main {
-
+  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int K [] = {2,4,5,7 ,10};
+		
+		MinMaxMod m =new MinMaxMod();
+		String randomState="0020000#0000000#0000000#0000000#0000000#0000000";
+		for(int i=0; i<K.length; i++) {
+			System.out.println("********************************");
+			System.out.println("At k= "+K[i]);
+			System.out.println("********");
+			System.out.println("alpha and Beta prunning :");
+			long start = System.currentTimeMillis();
+
+		     m.getNextMove(randomState, K[i], true);
+		    
+
+
+			long end = System.currentTimeMillis();
+			
+			System.out.println("Running Time(ms) :" + (end-start) );
+            System.out.println(" ");
+			System.out.println("Without alpha and Beta prunning :");
+
+			long start2 = System.currentTimeMillis();
+
+
+			m.getNextMove(randomState, K[i], false);
+
+
+			long end2 = System.currentTimeMillis();
+			System.out.println("Without alpha and Beta prunning Running Time(ms) :" + (end2-start2) );
+	
+		System.out.println("********************************");
+			
+		}
 
 	}
 	
